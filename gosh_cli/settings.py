a2005e36222ebff3bf59c02c5f19da72240fee22
Shihab Dider <shihabdider@gmail.com>
2026-04-29 14:43:14 -0400

    Remove legacy Python CLI

--- gosh_cli/settings.py
+++
@@ -1,9 +0,0 @@
-GENOME_MAP = {
-    "hg19": "GATK.GRCh37",  
-    "gatk.grch37": "GATK.GRCh37", 
-    "grch37": "GATK.GRCh37",
-    "hg38": "GATK.GRCh38", 
-    "gatk.grch38": "GATK.GRCh38",
-    "grch38": "GATK.GRCh38", 
-    "wmg-hg38": "WMG-hg38"
-}
